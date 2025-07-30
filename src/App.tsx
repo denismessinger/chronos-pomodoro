fa848cb7874b9249c042eced64d60225bee4c150
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-07-30 18:35:09 -0300

    Adicionado componente de menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './styles/theme.css';
-import './styles/global.css';
-
 import { Heading } from './components/Heading';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
+import './styles/theme.css';
+import './styles/global.css';
 
 export function App() {
   return (
     <>
       <Container>
         <Logo />
+      </Container>
+      <Container>
+        <Menu />
       </Container>
       <Container>
         <Heading>Menu</Heading>
