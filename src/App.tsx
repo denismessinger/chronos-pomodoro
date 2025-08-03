0f984009628741ad47d0c4869a55ed98101c9eda
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-08-03 15:16:35 -0300

    Adicionado rodape

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -41,9 +42,11 @@ export function App() {
           </div>
           <div className='formRow'>
             <DefaultButton color='green' icon={<PlayCircleIcon />} />
-            <DefaultButton color='red' icon={<StopCircleIcon />} />
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
