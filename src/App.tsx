4182b108c473279d8159227957e273e573e591a0
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-08-03 22:46:34 -0300

    Adicionadas paginas de notfound e about

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,7 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
+import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
 
 export function App() {
-  /* Lazy initialization serve para quando tiver calculos pesados 
-  const [numero, setNumero] = useState(() => {
-    return 0
-  }));
-  */
-
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown></CountDown>
-      </Container>
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='meuInput'
-              labelText='Qualquer Coisa'
-              type='text'
-              placeholder='Digite algo'
-            />
-          </div>
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className='formRow'>
-            <Cycles />
-          </div>
-          <div className='formRow'>
-            <DefaultButton color='green' icon={<PlayCircleIcon />} />
-          </div>
-        </form>
-      </Container>
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
