157d521853ee0f7f75325a81677472953c7ec7d7
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-08-07 20:56:52 -0300

    Criando estado para passar por props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currenctCycle: 0,
+  config: {
+    workTime: 0,
+    shortBreakTime: 0,
+    longBreakTime: 0,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
 import { Container } from '../../components/Container';
 import { CountDown } from '../../components/CountDown';
 import { MainForm } from '../../components/MainForm';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 import { MainTemplate } from '../../templates/MainTemplate';
+
+type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
 
 export function Home() {
   return (
