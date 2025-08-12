b26818efae6f47eb32344f43df5492c190f355dc
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-08-11 22:34:39 -0300

    Adicionado tipo do ciclo

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,10 +5,16 @@ import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
 import type { TaskModel } from '../../models/TaskModel';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
 
 export function MainForm() {
-  const { setState } = useTaskContext(0);
+  const { state, setState } = useTaskContext(0);
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  // ciclos
+  const newCycle = getNextCycle(state.currenctCycle);
+  const nextCycleType = getNextCycleType(newCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -29,7 +35,7 @@ export function MainForm() {
       completeDate: null,
       interruptDate: null,
       duration: 1,
-      type: 'workTime',
+      type: nextCycleType,
     };
 
     const secondsRemaining = newTask.duration * 60;
@@ -39,7 +45,7 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currenctCycle: 1,
+        currenctCycle: newCycle,
         secondsRemaining: secondsRemaining,
         formattedSecondsRemaining: '00:00',
         tasks: [...prevState.tasks, newTask],
