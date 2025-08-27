7ebb65049d39dc48edcf774386fd1da81f222be6
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-08-26 22:59:12 -0300

    Adicionada coluna para status da task

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,60 @@
+import { TrashIcon } from 'lucide-react';
+import { Container } from '../../components/Container';
+import { DefaultButton } from '../../components/DefaultButton';
+import { Heading } from '../../components/Heading';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+import styles from './style.module.css';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
+import { getTaskStatus } from '../../utils/getTaskStatus';
+
+export function History() {
+  const { state } = useTaskContext();
+
+  return (
+    <MainTemplate>
+      <Container>
+        <Heading>
+          <span>History</span>
+          <span className={styles.buttonContainer}>
+            <DefaultButton
+              icon={<TrashIcon />}
+              color='red'
+              aria-label='Apagar o histórico'
+              title='Apagar o histórico'
+            ></DefaultButton>
+          </span>
+        </Heading>
+      </Container>
+      <Container>
+        <div className={styles.responsiveTable}>
+          <table>
+            <thead>
+              <tr>
+                <th>Tarefa</th>
+                <th>Duração</th>
+                <th>Data</th>
+                <th>Status</th>
+                <th>Tipo</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.tasks.map(task => {
+                return (
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{task.type}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </Container>
+    </MainTemplate>
+  );
+}
