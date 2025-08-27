62c277d06351df450091a54945cd8c7b4c91b02b
Denis Messinger <56281783+denismessinger@users.noreply.github.com>
2025-08-26 21:15:35 -0300

    Adicionado router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import { Home } from './pages/Home';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './components/MessagesContainer';
-import { BrowserRouter, Routes, Route } from 'react-router';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { NotFound } from './pages/NotFound';
-import { About } from './pages/About';
+import { MainRouter } from './routers/MainRouter';
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about-pomodoro' element={<About />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
